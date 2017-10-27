1c187e213a2aad8ae8c9369099769a96e99c4920
jgrenat <jgrenat@users.noreply.github.com>
2017-10-27 14:33:34 +0200

    Live-coding starting point

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,7 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, p, program, text)
+import Html exposing (Html, button, div, program, text)
 import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
-import Random
 
 
 main : Program Never Model Msg
@@ -13,24 +11,10 @@ main =
 
 type Model
     = NoGame
-    | Game CoinState
-    | Result GameState
-
-
-type GameState
-    = Win
-    | Loss
-
-
-type CoinState
-    = Heads
-    | Tails
 
 
 type Msg
-    = FlipCoin
-    | CoinFlipped CoinState
-    | PlayerBet CoinState
+    = None
 
 
 init : ( Model, Cmd Msg )
@@ -40,65 +24,15 @@ init =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        FlipCoin ->
-            ( model, Random.generate CoinFlipped randomCoin )
-
-        CoinFlipped coinState ->
-            ( Game coinState, Cmd.none )
-
-        PlayerBet bet ->
-            let
-                result =
-                    if model == Game bet then
-                        Win
-                    else
-                        Loss
-            in
-                ( Result result, Cmd.none )
-
-
-randomCoin : Random.Generator CoinState
-randomCoin =
-    Random.map
-        (\b ->
-            if b then
-                Heads
-            else
-                Tails
-        )
-        Random.bool
+    ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    case model of
-        NoGame ->
-            div []
-                [ startGameButton ]
-
-        Game _ ->
-            div []
-                [ p [] [ text "Heads or Tails?" ]
-                , button [ class "btn btn-default", onClick (PlayerBet Heads) ] [ text "Heads" ]
-                , text " "
-                , button [ class "btn btn-default", onClick (PlayerBet Tails) ] [ text "Tails" ]
-                ]
-
-        Result state ->
-            let
-                result =
-                    if state == Win then
-                        "Congrats, you were right!"
-                    else
-                        "You failed..."
-            in
-                div []
-                    [ p [] [ text result ]
-                    , startGameButton
-                    ]
+    div []
+        [ startGameButton ]
 
 
 startGameButton : Html Msg
 startGameButton =
-    button [ class "btn btn-primary", onClick FlipCoin ] [ text "Start a new game" ]
+    button [ class "btn btn-primary" ] [ text "Start a new game" ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,31 +1,9 @@
 module Tests exposing (..)
 
-import Main exposing (CoinState(Heads, Tails), GameState(Loss, Win), Model(..), Msg(CoinFlipped, FlipCoin, PlayerBet), randomCoin, update, view)
 import Test exposing (..)
-import Test.Html.Query as Query
-import Expect
-import Random
-import Test.Html.Selector exposing (text)
-
-
-modelGameHead : Model
-modelGameHead =
-    Game Heads
 
 
 all : Test
 all =
     describe "Heads or Tails"
-        [ test "Guessing the correct answer should make the player win" <|
-            \_ ->
-                Expect.equal (update (PlayerBet Heads) modelGameHead) ( Result Win, Cmd.none )
-        , test "Guessing the wrong answer should make the player lose" <|
-            \_ ->
-                Expect.equal (update (PlayerBet Tails) modelGameHead) ( Result Loss, Cmd.none )
-        , test "Having winned a game should display a congratulation message" <|
-            \_ ->
-                view (Result Win) |> Query.fromHtml |> Query.has [ text "Congrats, you were right!" ]
-        , test "Having lost a game should display a message full of disappointment" <|
-            \_ ->
-                view (Result Loss) |> Query.fromHtml |> Query.has [ text "You failed..." ]
-        ]
+        []
