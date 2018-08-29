a1babba89ea8430f71fefc523ba1b15556539f52
jgrenat <jgrenat@users.noreply.github.com>
2018-08-29 22:22:20 +0200

    Upgrade result to 0.19

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,37 @@
 module Main exposing (Model(..), Msg(..), init, main, startGameButton, update, view)
 
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, p, text)
 import Browser exposing (element)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import Random
 
 
 main : Program () Model Msg
 main =
-    element { init = (\a -> init), view = view, update = update, subscriptions = \m -> Sub.none }
+    element { init = always init, view = view, update = update, subscriptions = \m -> Sub.none }
 
 
 type Model
     = NoGame
+    | Game CoinState
+    | Result GameState
 
 
 type Msg
-    = None
+    = FlipCoin
+    | CoinFlipped CoinState
+    | PlayerBet CoinState
+
+
+type GameState
+    = Win
+    | Loss
+
+
+type CoinState
+    = Head
+    | Tail
 
 
 init : ( Model, Cmd Msg )
@@ -25,15 +41,57 @@ init =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        FlipCoin ->
+            ( NoGame, Random.generate CoinFlipped randomCoin )
+
+        CoinFlipped coinState ->
+            ( Game coinState, Cmd.none )
+
+        PlayerBet coinState ->
+            let
+                result =
+                    if Game coinState == model then
+                        Win
+                    else
+                        Loss
+            in
+                ( Result result, Cmd.none )
+
+
+randomCoin : Random.Generator CoinState
+randomCoin =
+    Random.uniform Head [ Tail ]
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ startGameButton ]
+    case model of
+        NoGame ->
+            div []
+                [ startGameButton ]
+
+        Game coinState ->
+            div []
+                [ p [] [ text "Heads or Tails?" ]
+                , button [ class "btn btn-default", onClick (PlayerBet Head) ] [ text "Heads" ]
+                , button [ class "btn btn-default", onClick (PlayerBet Tail) ] [ text "Tails" ]
+                ]
+
+        Result state ->
+            let
+                result =
+                    if state == Win then
+                        "Congrats, you were right!"
+                    else
+                        "You failed..."
+            in
+                div []
+                    [ p [] [ text result ]
+                    , startGameButton
+                    ]
 
 
 startGameButton : Html Msg
 startGameButton =
-    button [ class "btn btn-primary" ] [ text "Start a new game" ]
+    button [ class "btn btn-primary", onClick FlipCoin ] [ text "Start a new game" ]
